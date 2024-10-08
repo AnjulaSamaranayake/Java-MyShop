7886fc7a792ca6e32fff5ac6c4ab870f9edece42
AnjulaSamaranayake <anjulasankalpa2255@gmail.com>
2024-10-08 17:10:07 +0530

    Update AppInitializer.java

--- AppInitializer.java
+++ AppInitializer.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 public class AppInitializer{
+
+    static String[][] user = new String[3][2];
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
@@ -26,5 +28,42 @@ public class AppInitializer{
             default:
             return;
         }
+    }
+
+    //Login Method
+
+    public static boolean login(){
+        return false;
+    }
+
+    //Register Method
+
+    public static boolean register(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insert your Email: ");
+        String email = input.nextLine();
+        System.out.println("Insert your Password: ");
+        String password = input.nextLine();
+
+        if(user[user.length-1][0] != null){
+            System.out.println("Sorry... Database is full...!");
+            return false;
+        }
+
+        for (int i = 0; i< user.length; i++){
+
+            if(user[i][0]==null){
+                user[i][0] = email;
+                user[i][1] = password;
+                System.out.println("Registration Successful!");
+                return true;
+            } else {
+                if(user[i][0].equalsIgnoreCase(email)) {
+                    System.out.println("Email already exists...!");
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
